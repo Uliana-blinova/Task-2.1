8c88cddb0b931cc441556086eabb78a7d754de3f
Uliana Blinova <ulanablinova0@gmail.com>
2025-11-18 19:14:53 +0700

    add admin function

--- main/admin.py
+++ main/admin.py
@@ -4,3 +4,19 @@ from .models import Category, Application
 
 admin.site.register(Category)
 admin.site.register(Application)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'description', 'user__username')
+    ordering = ('-created_at',)
+    # Чтобы изменять статус прямо в списке, можно использовать list_editable:
+    # list_editable = ('status',)
